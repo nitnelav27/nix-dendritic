275b8fda6521071de83e8ac655598e2170ce6d83
Valentín Vergara Hidd <valentinvergara@protonmail.com>
2026-05-29 14:00:44 -0400

    fixing agenix keys

--- secrets.nix
+++ secrets.nix
@@ -1,14 +1,15 @@
 let
   ## Personal public key, generated in nixtop
   valentin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9CaJu6FJJ4s4NaL546RufQdrw7UB4zlChTN10avrpt valentinvergara@gmail.com";
+  vvh-nixos-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEb/75yCQOiDvuAoRPEtC9L9T+QzvktxkSeC5qFuqFUI Valentín en NixOS-VM, Proxmox";
 
   ## The server's public host key (you can view this by running `cat /etc/ssh/ssh_host_ed25519_key.pub` on each host)
-  nixos-vm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGDsCDWlkOKolVms+OGo4a4Z6E9lJNhhNGHS+NPCU7K root@nixos-vm";
+  nixosVm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEGDsCDWlkOKolVms+OGo4a4Z6E9lJNhhNGHS+NPCU7K root@nixos-vm";
 
 in
   {
 
-  "secrets/cloudflare-ddns.age".publicKeys = [ valentin nixos-vm ];
-  "secrets/cloudflare-acme.age".publicKeys = [ valentin nixos-vm ];
+  "secrets/cloudflare-ddns.age".publicKeys = [ valentin vvh-nixos-vm nixosVm ];
+  "secrets/cloudflare-acme.age".publicKeys = [ valentin vvh-nixos-vm nixosVm ];
 
 }
